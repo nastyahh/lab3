5d5b9ae05580c6a5f71143012903414aec87eb5e
nastyahh <nasstyahus@gmail.com>
2024-02-17 20:16:46 +0300

    add vertical scrollingScreen

--- app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
+++ app/src/main/java/com/raywenderlich/android/lab1/screens/NavigationScreen.kt
@@ -39,6 +39,7 @@ fun NavigationScreen() {
             NavigationButton(stringResource(id = R.string.button_box), Screen.Box)
             NavigationButton(stringResource(id = R.string.button_surface), Screen.Surface)
             NavigationButton(stringResource(id = R.string.button_scaffold), Screen.Scaffold)
+            NavigationButton(stringResource(id = R.string.scrolling), Screen.Scrolling)
         }
     }
 }
